062a28b5ccfa026a812371ad497a8dd0e5aa3fdb
Sijan-Bhusal <sijan.bhusal03@gmail.com>
2026-02-09 20:20:58 +0545

    feat: configure global styles and site constants

---
+++ src/lib/constants.tsx
@@ -0,0 +1,20 @@
+// Consists of Company Contact Information or Other Static data that could be used across the websites
+
+export const COMPANY = {
+  name: "N0E9Tech",
+  email: "ntprocs@n0e9tech.com",
+  phone: "+977-9871234567",
+  address: "New Baneswor,Kathamandu",
+  city: "Kathmandu",
+  state: "Bagmati",
+  zip: "42006",
+} as const;
+
+export const STATS = {
+  services: "3+",
+  servicesLabel: "Core Service Areas",
+  technologies: "10+",
+  technologiesLabel: "Technologies Mastered",
+  response: "<24h",
+  responseLabel: "Response Time",
+} as const;
